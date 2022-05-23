9c80e72f97e8a881ccd636e102c45e46fd95039d
daehwa <daehwa001210@gmail.com>
2022-05-23 15:08:39 +0900

    fixed error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import type { NextPage } from "next";
+import Link from "next/link";
+
 import styled from "styled-components";
 import Board from "../components/board/Board";
+import Button from "../components/button/Button";
 
 const Container = styled.div`
   font-size: 21px;
@@ -11,6 +14,12 @@ const Index: NextPage = () => {
   return (
     <Container>
       <Board worry="안녕" />
+      <Link href={"/board/post/option" || ""}>
+        <a>
+          {/*onClickNext 추가 요망*/}
+          <Button color="dark_cyan">제출</Button>
+        </a>
+      </Link>
     </Container>
   );
 };
